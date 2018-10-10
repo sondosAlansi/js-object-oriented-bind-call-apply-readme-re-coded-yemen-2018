a99ab669eea6f2b003426db08a19414b43cd9349
sondos alansi <sondos.alansi@yahoo.com>
2018-10-10 18:53:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,12 @@ greet.call(sally);
 // my name is Sally, hi!
  
 greet.apply(sally);
+
+
+let sally = { name: 'Sally' };
+ 
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+ 
+greet.call(sally, 'Terry', 'George');
