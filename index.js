4fbdc79a989667614a7721da1c5feaa9c5df6e8e
sondos alansi <sondos.alansi@yahoo.com>
2018-10-10 18:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,3 +44,31 @@ newGreet('Bob');
 // Hi Bob, my name is Sally!
  
 greet('Bob');
+
+
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
