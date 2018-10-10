98e4b4bfc62f43e4d292a2caaadb9b10a6690528
sondos alansi <sondos.alansi@yahoo.com>
2018-10-10 18:53:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ let person = {
 };
  
 person.greet();
+
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+ 
+let sally = { name: 'Sally' };
+ 
+greet.call(sally);
+// my name is Sally, hi!
+ 
+greet.apply(sally);
